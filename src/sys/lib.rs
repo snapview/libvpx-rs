10b2e041056b17102af94a818e584fc9772ef258
Kornel Lesiński <kornel@geekhood.net>
2017-02-07 09:35:58 +0000

    Missing from vpx headers

--- src/sys/lib.rs
+++ src/sys/lib.rs
@@ -3,4 +3,33 @@
 #![allow(non_upper_case_globals)]
 #![allow(improper_ctypes)]
 
+// VP9
+#[repr(i32)]
+pub enum AQ_MODE {
+  NO_AQ = 0,
+  VARIANCE_AQ = 1,
+  COMPLEXITY_AQ = 2,
+  CYCLIC_REFRESH_AQ = 3,
+  EQUATOR360_AQ = 4,
+  // AQ based on lookahead temporal
+  // variance (only valid for altref frames)
+  LOOKAHEAD_AQ = 5,
+}
+
+impl Default for vpx_codec_ctx__bindgen_ty_1_vpx_codec_enc_cfg {
+    fn default() -> Self {
+        unsafe {::std::mem::zeroed()}
+    }
+}
+impl Default for vpx_codec_ctx {
+    fn default() -> Self {
+        unsafe {::std::mem::zeroed()}
+    }
+}
+impl Default for vpx_image_t {
+    fn default() -> Self {
+        unsafe {::std::mem::zeroed()}
+    }
+}
+
 include!(concat!(env!("OUT_DIR"), "/ffi.rs"));
