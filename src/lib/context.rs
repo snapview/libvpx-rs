e4839a24d5f68a7672005d3335f923115fea44e7
Daniel Abramov <dabramov@snapview.de>
2017-05-19 16:40:13 +0200

    Add simple bindings for libvpx context

---
+++ src/lib/context.rs
@@ -0,0 +1,34 @@
+use std::ops::{Deref, DerefMut};
+use ffi;
+
+pub struct CodecContext {
+    inner: ffi::vpx_codec_ctx_t
+}
+
+impl CodecContext {
+    pub fn new() -> Self {
+        CodecContext {
+            inner: Default::default()
+        }
+    }
+}
+
+impl Drop for CodecContext {
+    fn drop(&mut self) {
+        unsafe { ffi::vpx_codec_destroy(&mut self.inner) };
+    }
+}
+
+impl Deref for CodecContext {
+    type Target = ffi::vpx_codec_ctx_t;
+
+    fn deref(&self) -> &ffi::vpx_codec_ctx_t {
+        &self.inner
+    }
+}
+
+impl DerefMut for CodecContext {
+    fn deref_mut(&mut self) -> &mut ffi::vpx_codec_ctx_t {
+        &mut self.inner
+    }
+}
--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,5 +1,6 @@
 extern crate vpx_sys as ffi;
 
+mod context;
 mod error;
 mod image;
 
